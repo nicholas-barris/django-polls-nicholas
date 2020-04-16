7e01b2f3941f79c2c24ea5076af1e3400446e643
nicholas-barris <nicholasbarris@gmail.com>
2020-04-16 01:04:49 -0400

    Added question and choice models

--- src/nicholas_test_project/nicholas_test_project/settings.py
+++ src/nicholas_test_project/nicholas_test_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,13 +110,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
